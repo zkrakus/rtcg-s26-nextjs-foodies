57f34a6bc67eda9d9d9fe55b97fa9eb8b9b60c0b
zkrakus <zkrakus@gmail.com>
2025-08-13 12:57:15 -0400

    feat:A little cleanup.

--- app/page.js
+++ app/page.js
@@ -1,22 +1,26 @@
 import Link from "next/link";
 
-import classes from "./page/module.css";
+import classes from "./page.module.css";
+import ImageSlideshow from "@/components/images/image-slideshow";
 
 export default function Home() {
   return (
     <>
-      <header className={classes.header}></header>
-      <div className={classes.header}></div>
-      <div>
-        <div className={classes.hero}>
-          <h1>NextLevel Food for NextLevel Foodies</h1>
-          <p>Tase & share food from all over the world.</p>
+      <header className={classes.header}>
+        <div className={classes.slideshow}>
+          <ImageSlideshow />
         </div>
-        <div className={classes.cta}>
-          <Link href="/community">Join the Community</Link>
-          <Link href="/meals">Explore Meals</Link>
+        <div>
+          <div className={classes.hero}>
+            <h1>NextLevel Food for NextLevel Foodies</h1>
+            <p>Tase & share food from all over the world.</p>
+          </div>
+          <div className={classes.cta}>
+            <Link href="/community">Join the Community</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
         </div>
-      </div>
+      </header>
       <main>
         <section className={classes.section}>
           <h2>How it works</h2>
