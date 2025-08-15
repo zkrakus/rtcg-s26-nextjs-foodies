cdf52e868c8cf2c3f29027bcb1fe5a0cc80049ca
zkrakus <zkrakus@gmail.com>
2025-08-15 14:06:22 -0400

    feat:s6-l448 Added meals grid and meal item component. Added them to the meals page.

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,21 @@
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.moduel.css";
+
 export default function MealsPage() {
-  return <h1>Meals Page</h1>
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Delisiouc meals, created </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun.
+        </p>
+        <p className={classes.cta}>
+          <Link href="meals/share">Share your favorite recipe.</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
