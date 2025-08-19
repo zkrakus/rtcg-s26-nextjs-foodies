a08591b74599af2ab67aa1739cec50f3477f4c7f
zkrakus <zkrakus@gmail.com>
2025-08-19 14:58:24 -0400

    feat:s26-l455 Created some error handling for not found and generic fetching errors.

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,14 +2,19 @@ import MealsGrid from "@/components/meals/meals-grid";
 import classes from "./page.module.css";
 import { getMeals } from "@/lib/meals";
 import Link from "next/link";
+import { Suspense } from "react";
 
-export default async function MealsPage() {
+async function Meals(){
   const meals = await getMeals();
 
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
-        <h1>Delisiouc meals, created </h1>
+        <h1>Delicious meals, created </h1>
         <p>
           Choose your favorite recipe and cook it yourself. It is easy and fun.
         </p>
@@ -18,7 +23,9 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p className={classes.loading}> Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
---
+++ app/not-found.js
@@ -0,0 +1,6 @@
+export default function NotFound({props}){
+  return <main className="not-found">
+    <h1>Not found</h1>
+    <p>Unfortunately, we could not find the page or the requested resource.</p>
+  </main>
+}
