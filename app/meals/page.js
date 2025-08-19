5a17eed1df52beca6de24b3527594b0efa514cf5
zkrakus <zkrakus@gmail.com>
2025-08-19 14:31:25 -0400

    feat:s26-l451 Added lib folder for fetching meals from the db that has been server side rendered. Also adding loading page while fetching meals.

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,11 @@
 import MealsGrid from "@/components/meals/meals-grid";
-import classes from "./page.moduel.css";
+import classes from "./page.module.css";
+import { getMeals } from "@/lib/meals";
+import Link from "next/link";
 
-export default function MealsPage() {
+export default async function MealsPage() {
+  const meals = await getMeals();
+
   return (
     <>
       <header className={classes.header}>
@@ -14,7 +18,7 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
