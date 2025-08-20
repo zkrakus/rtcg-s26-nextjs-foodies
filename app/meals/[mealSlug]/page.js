e433e4c78355f8a441921a4ff074c14a53a6ea52
zkrakus <zkrakus@gmail.com>
2025-08-20 15:10:23 -0400

    feat:s26-l456 Added custom meal not found page..

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 export default function MealDetailspage({ params }) {
   const meal = getMeal(params.mealSlug);
+
+  if(!meal){
+    notFound();
+  }
 
   meal.instructions = meal.instructions.replace(/\n/g, '<br/>')
 
